53a3a36549f2f7b4df2e473e68a8eac813e95221
dorian-marchal <marchal.dorian@gmail.com>
2015-04-25 23:28:46 +0200

    Add a simple test page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 (function () {
     'use strict';
 
+    setInterval(function() {
+        var randomHex = parseInt((Math.random() * 16777215)).toString(16);
+        var randomColor = '#' + ('000000'.substring(0, 6 - randomHex.length) + randomHex);
+
+        var cards = document.getElementsByClassName('card');
+        cards[Math.floor(Math.random() * cards.length)].style.backgroundColor = randomColor;
+    }, 100);
+
     $(function () {
 
     });
-    
+
 })();
