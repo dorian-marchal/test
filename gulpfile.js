eff343d7c1075a23a964f0c782db31d3bea4075b
dorian-marchal <marchal.dorian@gmail.com>
2015-04-26 23:28:26 +0200

    Update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,41 @@
 // grab our gulp packages
 var gulp  = require('gulp');
 var gutil = require('gulp-util');
+var tinylr;
 
+
+gulp.task('default', ['express', 'livereload']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/css/*.css', ['copyCss']);
-    gulp.watch('src/js/*.js', ['copyJs']);
+    var cssFiles = 'src/css/*.css';
+    var jsFiles = 'src/js/*.js';
+    gulp.watch(cssFiles, ['copyCss']);
+    gulp.watch(jsFiles, ['copyJs']);
+    gulp.watch('assets/css/*.css', notifyLiveReload);
+});
+
+gulp.task('express', function() {
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')({ port: 35729 }));
+    app.use(express.static(__dirname));
+    app.listen(4000);
+});
+
+function notifyLiveReload(event) {
+    console.log('notifyLiveReload');
+    var fileName = require('path').relative(__dirname, event.path);
+
+    tinylr.changed({
+        body: {
+          files: [fileName]
+        }
+    });
+}
+
+gulp.task('livereload', ['watch'], function() {
+    tinylr = require('tiny-lr')();
+    tinylr.listen(35729);
 });
 
 gulp.task('copyJs', function() {
