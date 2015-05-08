0c208cf054c5bc5b774ee05b9ea788b9101d42bc
dorian-marchal <marchal.dorian@gmail.com>
2015-05-08 18:52:20 +0200

    Add leaflet map

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
     'use strict';
 
     $(function () {
-
+        L.map($('.map').get(0), {
+            center: new L.LatLng(51.505, -0.09),
+            zoom: 8,
+            layers: new L.TileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZG9yaWFuLW1hcmNoYWwiLCJhIjoiS2VBQTFZTSJ9.ikZEadF09jwZgQQnql7aMA'),
+        });
     });
-    
+
 })();
