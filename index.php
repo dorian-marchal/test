95510753b9304ca63639341be7afc974830dbb55
dorian-marchal <marchal.dorian@gmail.com>
2015-05-08 17:41:41 +0200

    Add leaflet

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Test base</title>
+    <title>Leaflet</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bower_components/leaflet/dist/leaflet.css">
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/leaflet/dist/leaflet.js"></script>
     <script src="js/script.js"></script>
 </head>
 
 <body>
+    <div class="map"></div>
 </body>
 </html>
