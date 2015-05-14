94c0ddbe4e0fb93b1453abbd362e9337ace1023c
dorian-marchal <marchal.dorian@gmail.com>
2015-05-14 23:28:17 +0200

    Improve leaflet test

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
     $(function () {
         L.map($('.map').get(0), {
-            center: new L.LatLng(51.505, -0.09),
+            center: new L.LatLng(48.8, 2.3),
             zoom: 8,
             layers: new L.TileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZG9yaWFuLW1hcmNoYWwiLCJhIjoiS2VBQTFZTSJ9.ikZEadF09jwZgQQnql7aMA'),
         });
