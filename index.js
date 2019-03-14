2f16210abf208d2830c0f1c70aa2ab8dbf99348d
Dorian Marchal <marchal.dorian@gmail.com>
2019-03-14 18:36:23 +0100

    step 6

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const asynk = require("./asynk");
 const fetch = require("node-fetch");
 
-const getTvChannel = async function(id) {
-  const res = await fetch(`https://api.jvc.gg/tvChannel/${id}`);
-  return await res.json();
-};
+const getTvChannel = asynk(function*(id) {
+  const res = yield fetch(`https://api.jvc.gg/tvChannel/${id}`);
+  return yield res.json();
+});
 
 getTvChannel(1).then(console.log);
